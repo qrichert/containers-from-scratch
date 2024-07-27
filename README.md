db947e6c987b41cc09ff360260a4cddf91eb6f51
Quentin Richert <quentin@richert.co>
2024-07-27 21:30:13 +0200

    feat: v0

---
+++ README.md
@@ -0,0 +1,20 @@
+# Containers From Scratch
+
+_Create a Docker container, from scratch._
+
+## Interactive demo
+
+```console
+$ git switch demo/c
+$ git slides start
+$ ./dockus run [command]
+```
+
+For `git slides`, see
+[qrichert/git-slides](https://github.com/qrichert/git-slides/).
+
+## Sources
+
+- [Building a container from scratch in Go - Liz Rice (Microscaling Systems)](https://www.youtube.com/watch?v=Utf-A4rODH8)
+- [Containers From Scratch • Liz Rice • GOTO 2018](https://www.youtube.com/watch?v=8fi7uSYlOdc)
+- <https://gist.github.com/lizrice/a5ef4d175fd0cd3491c7e8d716826d27>
---
+++ src/main.c
@@ -0,0 +1,49 @@
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/mount.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sched.h>
+
+void run(int, char*[]);
+
+// docker run <container> <command> <args>
+// dockus run             <command> <args>
+int main(int argc, char* argv[]) {
+    if (argc >= 3 && strcmp(argv[1], "run") == 0) {
+        run(argc - 2, &argv[2]);
+    } else {
+        fprintf(stderr, "Bad arguments.\n");
+        return 2;
+    }
+    return 0;
+}
+
+void run(int argc, char* argv[]) {
+    char* args[argc + 1];
+    memcpy(args, argv, argc * sizeof(char*));
+    args[argc] = NULL; // Args to `execvp()` must be NULL-terminated.
+
+    unshare(
+        CLONE_NEWUTS // Hostname.
+        | CLONE_NEWPID // PID.
+        | CLONE_NEWNS // Mounts.
+    );
+
+    sethostname("dockus", strlen("dockus"));
+
+    chroot("./alpine_fs");
+    chdir("/");
+
+    pid_t pid = fork();
+    if (pid == 0) {
+        mount("proc", "/proc", "proc", 0, NULL);
+        execvp(args[0], args);
+        fprintf(stderr, "'%s' not found.\n", args[0]);
+    } else {
+        waitpid(pid, NULL, 0);
+        umount("/proc");
+    }
+}
